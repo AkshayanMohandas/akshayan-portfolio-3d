8bced28813e8d012dc10a3d107356fbfa20338d7
AkshayanMohandas <akshayanmohandas@gmail.com>
2025-10-26 21:50:07 +0000

    Add Webpack configuration
    - Configured webpack for ES6+ transpilation with Babel
    - Set up development server on port 8080
    - Added build output configuration for production

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['@babel/polyfill', './src/app.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, ''),
+    compress: true,
+    watchContentBase: true,
+    port: 8080,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
+  node: {
+    fs: 'empty',
+  },
+};
